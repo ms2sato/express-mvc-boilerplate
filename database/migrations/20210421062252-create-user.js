4f8c55cfec777c84161ee96b012b948e2e92289d
ms2sato <ms2.sato@gmail.com>
2021-04-21 16:26:17 +0900

    user model

---
+++ database/migrations/20210421062252-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      provider: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      uid: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      displayName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.addIndex('Users', ['provider', 'uid'], { unique: true });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeIndex('Users', ['provider', 'uid']);
+    await queryInterface.dropTable('Users');
+  }
+};
