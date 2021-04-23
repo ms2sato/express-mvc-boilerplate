319a20740ef64d7890df3b10c95ebda5b5511cca
ms2sato <ms2.sato@gmail.com>
2021-04-23 10:31:20 +0900

    add resource controller logics

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ route.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', user: req.user });
 });
 
-route.get('/posts', 'posts_controller@index');
-route.get('/posts/:id', 'posts_controller@show');
+// single style
+// route.get('/posts', 'posts_controller@index');
+// route.get('/posts/:id', 'posts_controller@show');
+
+// resource style
+route.resource('posts', 'posts_controller');
 
 module.exports = route.router;
