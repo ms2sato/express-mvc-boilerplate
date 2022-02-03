3a16cb4021afa681f3f7f539a83fd4f9f27353c6
Masashi Sato <ms2.sato@gmail.com>
2022-02-03 21:36:49 +0900

    regenerate session id on logging in

--- routes/auth.js
+++ routes/auth.js
@@ -22,30 +22,52 @@ passport.use(new LocalStrategy(
   async (username, password, done) => {
     try {
       const user = await models.User.authenticate({ username, password });
-      done(null, user, { message: 'ログインしました' });
+      done(null, user);
     } catch (err) {
-      if(err.errors) {
-        const message = `${err.message}: ${err.errors.map(error => error.message).join(' ')}`;
-        return done(null, false, { message });
-      } else {
-        return done(err, null);
-      }
+      done(err, null);
     }
   }
 ));
 
-router.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    successFlash: { type: 'info' }, // req.flash('info', message) if success
-    failureFlash: { type: 'alert' }, // req.flash('alert', message) if fail
-    badRequestMessage: 'ユーザ名、パスワードを入力してください' // unless username || password
-  })
-);
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {}, (err, user) => {
+    if (err) {
+      if (err.errors) {
+        return res.render('login', { err });
+      } else {
+        return next(err);
+      }
+    }
+
+    if (!user) {
+      return res.render('login', {
+        user: req.user, err: new Error('ユーザ名、パスワードを入力してください')
+      });
+    }
+
+    // @see https://qiita.com/KeitaMoromizato/items/55c35a5d6d039aa7a385
+    req.session.regenerate((err) => {
+      if (err) {
+        console.error(err);
+        return next(err);
+      }
+
+      req.login(user, (err) => {
+        if (err) {
+          console.error(err);
+          return next(err);
+        }
+
+        req.flash('info', 'ログインしました');
+        res.redirect('/');
+      });
+    });
+
+  })(req, res, next);
+});
 
 router.get('/login', (req, res, _next) => {
-  res.render('login', { user: req.user });
+  res.render('login');
 });
 
 router.get('/logout', (req, res, _next) => {
